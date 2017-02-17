31ac0a0317fc37ab593f805f5619462450015a6a
Josip <josip.susic@hotmail.com>
2017-02-17 14:05:17 +0100

    backup files

---
+++ footer.php
@@ -0,0 +1,35 @@
+		<footer>
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-8">
+						<div class="row">
+							<?php dynamic_sidebar('footer_widget'); ?>
+						</div>
+					</div>
+					<div class="col-sm-4">
+						<h3>Follow Us</h3>
+						<?php 
+							wp_nav_menu(array(
+								'theme_location' => 'social_menu',
+								'container' => 'div',
+								'container_id' => 'menu-social',
+								'container_class' => 'menu',
+								'menu_id' => 'social',
+								'menu_class' => 'menu-items',
+								'depth' => 1, //wont print child elements
+								'link_before' => '<span class="sr-only">', //for screen readers
+								'link_after' => '</span>',
+								'fallback_cb' => '',
+								'items_wrap' => '<ul id="%1$s" class="%2$s">%3$s</ul>'
+							));
+						?>
+					</div>
+				</div>
+			</div>
+			<p class="text-center site-info">
+				The Travel Blog <?php echo date('Y'); ?>
+			</p>
+		</footer>
+		<?php wp_footer(); ?> <!-- load js file at the bottom -->
+	</body>
+</html>
---
+++ header.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+	<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>> <!-- print body classes -->
+	<?php $featured = wp_get_attachment_image_src(get_post_thumbnail_id(), 'full'); ?>
+	<!--pre>
+		<?php //var_dump($featured); ?>
+	</pre-->
+	<?php $featured = $featured[0]; ?>
+
+	<header class="site-header" style="background-image:url(<?php echo $featured ?>);">
+		<nav class="main-navigation">
+			<div class="container">
+				<div class="row">
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false">
+							<span class="sr-only">Toggle Navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<a href="<?php echo esc_url(home_url('/')) ?>">
+							<img src="<?php echo get_stylesheet_directory_uri() ?>/img/logo.png" class="img-responsive">
+					    </a>
+					</div>
+					<div class="navbar-right">
+						<?php wp_nav_menu(array(
+							'theme_location' => 'main_menu',
+							'container_id' => 'navbar',
+							'container_class' => 'collapse navbar-collapse',
+							'menu_class' => 'nav navbar-nav navbar-right'
+						)); ?>
+					</div>
+				</div> <!--/.row -->
+			</div> <!--/.container -->
+		</nav> <!--/.main-navigation -->
+
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6">
+					<div class="page-title">
+						
+						<?php if(is_front_page()): ?>
+							<?php $description = get_bloginfo('description', 'display'); ?>
+							<h1 class="title"><span><?php echo $description; ?></span></h1>
+						<?php elseif (is_category()): ?>
+							<h1 class="title"><span><?php echo the_archive_title(); ?></span></h1>
+						<?php else: ?>
+							<h1 class="title"><span><?php echo the_title(); ?></span></h1>
+					    <?php endif; ?>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
